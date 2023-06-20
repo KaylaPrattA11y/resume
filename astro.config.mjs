179465508d7d56178ed5e106817dc8fe309b07d4
kayla.pratt <kayla.pratt@fleetcor.com>
2023-06-20 17:08:36 -0400

    deploy with github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kaylapratta11y.github.io',
+  base: '/resume',
+});
